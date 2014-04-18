e76d3eeb60e400d71f13f72c45e2e447ea9b64fe
Jacob <kula_dkj@hotmail.com>
2014-04-18 17:36:12 +0800

    add test

--- src/main/java/com/kula/qrplayer/qrcode/BinaryQrCodeWriter.java
+++ src/main/java/com/kula/qrplayer/qrcode/BinaryQrCodeWriter.java
@@ -39,42 +39,30 @@ public class BinaryQrCodeWriter {
         return renderResult(qrCode, width, height, QUIET_ZONE_SIZE);
     }
 
-    /**
-     * Render result.
-     *
-     * @param code the code
-     * @param width the width
-     * @param height the height
-     * @param quietZone the quiet zone
-     * @return the bit matrix
-     */
-    private BitMatrix renderResult(final QRCode code, final int width, final int height,
-            final int quietZone) {
-        // Note that the input matrix uses 0 == white, 1 == black, while the output matrix uses
-        // 0 == black, 255 == white (i.e. an 8 bit greyscale bitmap).
+    // Note that the input matrix uses 0 == white, 1 == black, while the output matrix uses
+    // 0 == black, 255 == white (i.e. an 8 bit greyscale bitmap).
+    private static BitMatrix renderResult(QRCode code, int width, int height, int quietZone) {
         ByteMatrix input = code.getMatrix();
         if (input == null) {
             throw new IllegalStateException();
         }
-        final int inputWidth = input.getWidth();
-        final int inputHeight = input.getHeight();
-        final int qrWidth = inputWidth + (quietZone << 1);
-        final int qrHeight = inputHeight + (quietZone << 1);
-        final int outputWidth = Math.max(width, qrWidth);
-        final int outputHeight = Math.max(height, qrHeight);
+        int inputWidth = input.getWidth();
+        int inputHeight = input.getHeight();
+        int qrWidth = inputWidth + (quietZone << 1);
+        int qrHeight = inputHeight + (quietZone << 1);
+        int outputWidth = Math.max(width, qrWidth);
+        int outputHeight = Math.max(height, qrHeight);
 
-        final int multiple = Math.min(outputWidth / qrWidth, outputHeight / qrHeight);
+        int multiple = Math.min(outputWidth / qrWidth, outputHeight / qrHeight);
         // Padding includes both the quiet zone and the extra white pixels to accommodate the
         // requested
         // dimensions. For example, if input is 25x25 the QR will be 33x33 including the quiet zone.
         // If the requested size is 200x160, the multiple will be 4, for a QR of 132x132. These will
         // handle all the padding from 100x100 (the actual QR) up to 200x160.
-        final int leftPadding = (outputWidth - (inputWidth * multiple)) / 2;
-        final int topPadding = (outputHeight - (inputHeight * multiple)) / 2;
+        int leftPadding = (outputWidth - (inputWidth * multiple)) / 2;
+        int topPadding = (outputHeight - (inputHeight * multiple)) / 2;
 
-        public foolean BitMatrix output = new BitMatrix(outputWidth, outputHeight);{
-            
-        }
+        BitMatrix output = new BitMatrix(outputWidth, outputHeight);
 
         for (int inputY = 0, outputY = topPadding; inputY < inputHeight; inputY++, outputY += multiple) {
             // Write the contents of this row of the barcode
